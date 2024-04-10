951fa5872b7600feffba0c068e19eff57d018976
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-11 01:54:56 +0300

    Update loop.py

--- VIPMUSIC/plugins/admins/loop.py
+++ VIPMUSIC/plugins/admins/loop.py
@@ -8,13 +8,14 @@ from VIPMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["loop", "cloop"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["loop", "تكرار"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @AdminRightsCheck
 async def admins(cli, message: Message, _, chat_id):
     usage = _["admin_17"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip()
+    user_mention = message.from_user.mention if message.from_user else "المشـرف"
     if state.isnumeric():
         state = int(state)
         if 1 <= state <= 10:
@@ -30,13 +31,13 @@ async def admins(cli, message: Message, _, chat_id):
             )
         else:
             return await message.reply_text(_["admin_17"])
-    elif state.lower() == "enable":
+    elif state.lower() == "تفعيل":
         await set_loop(chat_id, 10)
         return await message.reply_text(
             text=_["admin_18"].format(state, message.from_user.mention),
             reply_markup=close_markup(_),
         )
-    elif state.lower() == "disable":
+    elif state.lower() == "تعطيل":
         await set_loop(chat_id, 0)
         return await message.reply_text(
             _["admin_19"].format(message.from_user.mention),
