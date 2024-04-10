855d42477807422793a245adfc6a6c6b652bb98b
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-10 20:33:05 +0300

    Update play.py

--- VIPMUSIC/utils/inline/play.py
+++ VIPMUSIC/utils/inline/play.py
@@ -67,7 +67,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
 
         [
             InlineKeyboardButton(
-                text=_["S_B_5"],
+                text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             ),
         ],
@@ -92,7 +92,7 @@ def stream_markup(_, videoid, chat_id):
 
         [
             InlineKeyboardButton(
-                text=_["S_B_5"],
+                text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             ),
         ],
@@ -139,7 +139,7 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
         [
            InlineKeyboardButton(
 
-                text=_["S_B_5"],
+                text=_["S_B_1"],
 
                 url=f"https://t.me/{app.username}?startgroup=true",
 
@@ -245,7 +245,7 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         [
            InlineKeyboardButton(
 
-                text=_["S_B_5"],
+                text=_["S_B_1"],
 
                 url=f"https://t.me/{app.username}?startgroup=true",
 
@@ -302,7 +302,7 @@ def queue_markup(_, videoid, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_5"],
+                text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             ),
         ],
@@ -330,7 +330,7 @@ def stream_markup2(_, chat_id):
     buttons = [
           [
             InlineKeyboardButton(
-                text=_["S_B_5"],
+                text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             ),
           ],
