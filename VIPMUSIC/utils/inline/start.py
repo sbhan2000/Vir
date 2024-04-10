c02f76b860e50145558d475ceb5e628c969e93a7
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-10 21:21:55 +0300

    Update start.py

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -1,87 +1,42 @@
-from typing import Union
-
+from pyrogram.types import InlineKeyboardButton
 from config import SUPPORT_CHANNEL, SUPPORT_CHAT
+
+import config
 from VIPMUSIC import app
-import asyncio
-import re
-import os
-import random
-import requests
-from pyrogram import Client, filters
-from pyrogram.types import (
-    CallbackQuery,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    ReplyKeyboardMarkup,
-    ReplyKeyboardRemove,
-    InputMediaPhoto,
-    Message,
-)
-from pyrogram import filters
 
 
-def start_pannel(_):
+def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_10"], callback_data="settings_helper"),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["S_B_10"], callback_data="settings_helper"
+            ),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
     ]
-    if SUPPORT_CHANNEL and SUPPORT_CHAT:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_2"], url=f"{SUPPORT_CHAT}"),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}")]
-            )
-        if SUPPORT_CHAT:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_2"], url=f"{SUPPORT_CHAT}")]
-            )
     return buttons
 
 
-def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def private_panel(_):
     buttons = [
-        [InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper")]
-    ]
-    if SUPPORT_CHANNEL and SUPPORT_CHAT:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_2"], url=f"{SUPPORT_CHAT}"),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}")]
-            )
-        if SUPPORT_CHAT:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_2"], url=f"{SUPPORT_CHAT}")]
-            )
-    buttons.append(
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             )
-        ]
-    )
-    if OWNER:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_5"], user_id=OWNER),
-            ]
-        )
-    buttons.append([InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG")])
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}"),
+            InlineKeyboardButton(text=_["S_B_2"], url=f"{SUPPORT_CHAT}"),
+          ], 
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+        ],
+    ]
     return buttons
