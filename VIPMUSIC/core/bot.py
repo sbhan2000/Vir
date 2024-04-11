0bd90a811235a085a4aba6416ded2c010938f93f
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-11 12:14:08 +0300

    Update bot.py

--- VIPMUSIC/core/bot.py
+++ VIPMUSIC/core/bot.py
@@ -31,23 +31,23 @@ class VIP(Client):
                 text=f"<u><b>» {self.mention} 🥤| تـم تـشغيل بـوت :</b><u>\n\n🥤|أيـدي الـبوت : <code>{self.id}</code>\n\n🥤| اسـم الـبوت : {self.name}\n\n🥤| يـوزر الـبوت : @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            LOGGER(𝚂𝙾𝚄𝚁𝙲𝙴 𝙷𝙼𝙳).error(
+            LOGGER(__name__).error(
                 "قـم بـأضافة البوت الى المجموعه ورفعه مشرف وفتح اتصال"
             )
             
         except Exception as ex:
-            LOGGER(𝚂𝙾𝚄𝚁𝙲𝙴 𝙷𝙼𝙳).error(
+            LOGGER(__name__).error(
                 f"لـم يـسطتع الـبوت الـوصول الى الـمجموعه.\n  بسبب : {type(ex).__name__}."
             )
             
 
         a = await self.get_chat_member(config.LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(𝚂𝙾𝚄𝚁𝙲𝙴 𝙷𝙼𝙳).error(
+            LOGGER(__name__).error(
                 "مـن فضلك قـم بـرفع الـبوت مـشرف وفـتح اتـصال. "
             )
             
-        LOGGER(𝚂𝙾𝚄𝚁𝙲𝙴 𝙷𝙼𝙳).info(f"🥤| تـم تـشغيل بـوت {self.name} بـنجاح. ")
+        LOGGER(__name__).info(f"🥤| تـم تـشغيل بـوت {self.name} بـنجاح. ")
 
     async def stop(self):
         await super().stop()
