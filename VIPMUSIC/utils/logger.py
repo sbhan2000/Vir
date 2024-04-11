739b3d9d2023be3a798a47e4efddfb4d8f15221e
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-11 17:57:05 +0300

    Update logger.py

--- VIPMUSIC/utils/logger.py
+++ VIPMUSIC/utils/logger.py
@@ -1,35 +1,49 @@
-from pyrogram.enums import ParseMode
+# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
 
-from VIPMUSIC import app
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
+from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
 from VIPMUSIC.utils.database import is_on_off
-from config import LOG_GROUP_ID
+from VIPMUSIC import app
 
 
 async def play_logs(message, streamtype):
-    if await is_on_off(2):
+    if await is_on_off(LOG):
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
         logger_text = f"""
-<b>•────‌‌‏──‌‌‏─‌‌‏─‌‌‏─‌‌‏─‌‌‏─‌‌‏─•</b>
-<b>إشعـارات  الميـوزك 𝄞</b>
-<b>•────‌‌‏─‌‌‏─‌‌‏─‌‌‏─‌‌‏──‌‌‏─‌‌‏─•</b>
-<b>🥤| سيـدي المطـور</b>
-<b>🥤| هنـاك شخص يستخـدم بوت {app.mention}</b>
-
-<b>🥤| أيـدي الـكروب :</b> <code>{message.chat.id}</code>
-<b>🥤| أسـم الـكروب :</b> {message.chat.title}
-<b>🥤| يـوزر الكروب :</b> @{message.chat.username}
-
-<b>🥤| أيـدي الـعضو :</b> <code>{message.from_user.id}</code>
-<b>🥤| اسـم الـعضو :</b> {message.from_user.mention}
-<b>🥤| يـوزر الـعضو :</b> @{message.from_user.username}
-
-<b>🥤| تـم تـشغيل :</b> {message.text.split(None, 1)[1]}
-<b>🥤| نـوع التشغيل :</b> {streamtype}"""
-        if message.chat.id != LOGGER_ID:
+**━━━━━━━━━━━━━━━**
+**💞 {MUSIC_BOT_NAME} ᴍᴜsɪᴄ ʟᴏɢs **
+**━━━━━━━━━━━━━━━**
+**🌹️ 𝐂𝐡𝐚𝐭 𝐍𝐚𝐌𝐞 : >** {message.chat.title} [`{message.chat.id}`]
+**━━━━━━━━━━━━━━━**
+**🥀 𝐍𝐚𝐌𝐞 : ›** {message.from_user.mention}
+**━━━━━━━━━━━━━━━**
+**🌸 𝐔𝐬𝐞𝐑𝐧𝐚𝐌𝐞 : ›** @{message.from_user.username}
+**━━━━━━━━━━━━━━━**
+**🌷 𝐈𝐃  : ›** `{message.from_user.id}`
+**━━━━━━━━━━━━━━━**
+**🌿 𝐂𝐡𝐚𝐭 𝐥𝐢𝐧𝐤: >** {chatusername}
+**━━━━━━━━━━━━━━━**
+**🌻 𝐒𝐞𝐀𝐫𝐜𝐇𝐞𝐝 𝐅𝐨𝐫:** {message.text}
+**━━━━━━━━━━━━━━━**
+**💐 𝐒𝐭𝐫𝐄𝐚𝐦 𝐓𝐲𝐏𝐞:** {streamtype}
+**━━━━━━━━━━━━━━━**"""
+        if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
-                    chat_id=LOG_GROUP_ID,
-                    text=logger_text,
-                    parse_mode=ParseMode.HTML,
+                    LOG_GROUP_ID,
+                    f"{logger_text}",
                     disable_web_page_preview=True,
                 )
             except:
